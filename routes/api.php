5f748057f7763b89aed545dc306e438a4fcf11c1
LizandroBackEnd <codeversebackend@gmail.com>
2025-11-02 08:03:03 -0600

    ci(Routes): Configured the routes by rol

--- routes/api.php
+++ routes/api.php
@@ -16,13 +16,10 @@ use Illuminate\Support\Facades\Route;
 //public routes
 Route::post('/register', [AuthController::class,  'register']);
 Route::post('/login', [AuthController::class,  'login']);
+Route::post('/logout', [AuthController::class, 'logout']);
 
 //private routes
 Route::middleware([IsAdmin::class])->group((function () {
-    Route::controller(AuthController::class)->group(function () {
-        Route::post('/logout', 'logout');
-        Route::get('/me', 'me');
-    });
     Route::controller(ServiceController::class)->group(function () {
         Route::get('/services', 'getServices');
         Route::get('/services/{name}', 'getServiceByName');
@@ -32,10 +29,10 @@ Route::middleware([IsAdmin::class])->group((function () {
     });
     Route::controller(EmployeesController::class)->group(function () {
         Route::get('/employees', 'getEmployees');
-        Route::get('/employee/{email}', 'getEmployeeByEmail');
+        Route::get('/employees/{email}', 'getEmployeeByEmail');
         Route::post('/employees', 'addEmployee');
-        Route::patch('/employee/{id}', 'updateEmployeeById');
-        Route::delete('/employee/{id}', 'deleteEmployeeById');
+        Route::patch('/employees/{id}', 'updateEmployeeById');
+        Route::delete('/employees/{id}', 'deleteEmployeeById');
     });
     Route::controller(ClientController::class)->group(function () {
         Route::get('/clients', 'getClients');
@@ -58,16 +55,25 @@ Route::middleware([IsAdmin::class])->group((function () {
 }));
 
 Route::middleware([IsReceptionist::class])->group((function () {
-    Route::controller(AuthController::class)->group(function () {
-        Route::post('/logout', 'logout');
-        Route::get('/me', 'me');
+    Route::controller(ClientController::class)->group(function () {
+        Route::get('/clients', 'getClients');
+        Route::get('/clients/{email}', 'getClientByEmail');
+        Route::post('/clients', 'addClient');
+        Route::patch('/clients/{id}', 'updateClientById');
+        Route::delete('/clients/{id}', 'deleteClientById');
+    });
+    Route::controller(BreedController::class)->group(function () {
+        Route::get('/breeds/{species}', 'getBreedBySpecies');
+        Route::post('/breeds', 'addBreed');
+    });
+    Route::controller(PetController::class)->group(function () {
+        Route::get('/pets', 'getPets');
+        Route::get('/pets/{name}', 'getPetByName');
+        Route::post('/pets', 'addPet');
+        Route::patch('/pets/{id}', 'updatePetById');
+        Route::delete('/pets/{id}', 'deletePetById');
     });
 }));
 
 
-Route::middleware([IsVeterinarian::class])->group((function () {
-    Route::controller(AuthController::class)->group(function () {
-        Route::post('/logout', 'logout');
-        Route::get('/me', 'me');
-    });
-}));
+Route::middleware([IsVeterinarian::class])->group((function () {}));
